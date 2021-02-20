87050775be9cbc39d5742b43b1aee5b47db822ac
Hsiao Yao <tomtogo1260@gmail.com>
2021-02-21 04:18:10 +0800

    :tada: initial commit
    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">
+Cube Transform | <a href="https://tomhsiao1260.github.io/cube-transform/" target="_blank">Play It<a/>
+</h1>
+
+<p align="center">
+    <img src="./src/assets/cube-transform.gif" width="500px"/>
+</p>
+
+## Introduction
+
+<h4 align="justify">
+Try to add some damping features to the mesh transformation. Welcome to adjust the parameters through dat.GUI for further use in your own projects 🎉
+<h4/>
+
+<h4>
+keywords： Three.js, Mesh Transformation, Damping Motion, Raycaster
+<h4/>
+
+## Installation
+
+```
+npm install
+```
+
+## Start Server
+
+```
+npm run dev
+```
+
+## Start Server
+
+```
+npm run dev
+```
+
+## Notes
+
+<p>Would be really appreciated if you are willing to give me a star 🎉</p>
+<p>If you have any questions or somewhere I can do better, welcome to send an issue or e-mail to me 🙏</p>
+
+e-mail: tomtogo1260@gmail.com
+
---
+++ src/script.js
@@ -0,0 +1,137 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Dat GUI
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    new THREE.MeshNormalMaterial()
+)
+
+scene.add(cube)
+
+
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(1, 1, 2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// GUI
+const parameters = {
+    radius: 1.0,
+    moveSpeed: 0.01,
+    damping: 0.1,
+    tile: 10,
+}
+
+gui.add(parameters, 'moveSpeed').min(0.01).max(0.1).step(0.01)
+gui.add(parameters, 'damping').min(0).max(1).step(0.01)
+gui.add(parameters, 'tile').min(0).max(10).step(0.1)
+
+// Raycaster
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+
+// Desktop
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+})
+// Mobile
+window.addEventListener('touchstart', (event) => {
+    mouse.x = event.touches[0].clientX / sizes.width * 2 - 1
+    mouse.y = - (event.touches[0].clientY / sizes.height) * 2 + 1
+})
+
+// Animate
+const clock = new THREE.Clock()
+let velocity = new THREE.Vector3()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    raycaster.setFromCamera(mouse, camera)
+
+    // Ray direction vector
+    const ray = raycaster.ray.direction
+    // Calculate the displacement vector between the cube and the final location
+    const xDelta = camera.position.clone()
+                                  .add( ray.normalize() )
+                                  .sub( new THREE.Vector3() )
+                                  .normalize()
+                                  .multiplyScalar( parameters.radius )
+                                  .sub( cube.position )
+
+    // Note: cube is always trapped inside an invisible sphere
+
+    // Acceleration from the drag force
+    const aDrag = xDelta.clone()
+                        .multiplyScalar( parameters.moveSpeed )
+    // Acceleration from the damping force
+    const aDamp = velocity.clone()
+                          .multiplyScalar( parameters.damping )
+
+    // Change the velocity
+    velocity = velocity.add( aDrag )
+                       .sub( aDamp )
+
+    // Move the cube
+    cube.position.add( velocity )
+
+    // Tilt the cube
+    const {x, y, z} = velocity.clone().multiplyScalar( parameters.tile )
+    cube.rotation.set(-z, x, y)
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
